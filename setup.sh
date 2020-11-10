e36f5a9106352aad4eaf8ce4a9ef323aafbde672
psawa <mkbloody@gmail.com>
2020-11-11 00:00:11 +0100

    conditionnal download of xlnet

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 pip install -r requirements.txt
 
-(mkdir -p application/models/gector/data/model_files && cd application/models/gector/data/model_files && curl -O https://grammarly-nlp-data-public.s3.amazonaws.com/gector/xlnet_0_gector.th)
+
+FILE=application/models/gector/data/model_files/xlnet_0_gector.th
+if [ -f "$FILE" ]; then
+    echo "$FILE is alreday downloaded."
+else 
+    (mkdir -p application/models/gector/data/model_files && cd application/models/gector/data/model_files && curl -O https://grammarly-nlp-data-public.s3.amazonaws.com/gector/xlnet_0_gector.th)
+fi
