0e4cb2746def1b21fda428997988c8e8eae45317
psawa <38402230+psawa@users.noreply.github.com>
2020-12-09 16:55:14 +0100

    typo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 
 FILE=application/models/gector/data/model_files/xlnet_0_gector.th
 if [ -f "$FILE" ]; then
-    echo "$FILE is alreday downloaded."
+    echo "$FILE is already downloaded."
 else 
     (mkdir -p application/models/gector/data/model_files && cd application/models/gector/data/model_files && curl -O https://grammarly-nlp-data-public.s3.amazonaws.com/gector/xlnet_0_gector.th)
 fi
