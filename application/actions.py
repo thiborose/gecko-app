7b62cf0f5aa42591bd3a2afabd37abc189c51181
jacqle <jacqmin.leo@gmail.com>
2020-12-09 15:39:20 +0100

    fixes #7

--- application/actions.py
+++ application/actions.py
@@ -1,27 +1,27 @@
 from application import nlp, model
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from application.models.gector.model import load_model
-from application.models.gector.predict import predict_for_tokens, predict_for_string
+from application.models.gector.predict import predict_for_sentences
+from application.models.gector.utils.preprocess_data import align_sequences
+
+def predict(input_text: str) -> str:
+    """Returns the predicted correction for an input text."""
+
+    tokenized_sentences = tokenize_and_segment(input_text)
+    corrected_sentences = predict_for_sentences(tokenized_sentences, model)
+    output_text = untokenize(corrected_sentences)
+    # sent_with_tags = align_sequences(input_string, output_text)
+    return output_text 
 
 
-def predict(input_string):
-    tokenized_string = tokenize(input_string)
-    corrected_text = predict_for_string(tokenized_string, model)
-    output_text = untokenize(corrected_text)
-    return output_text
+def tokenize_and_segment(input_text: str) -> list:
+    """Returns a list of tokenized sentences (strings)."""
 
+    doc = nlp(input_text)
+    sentences = []
+    for sent in doc.sents:
+        sentences.append(' '.join(token.text for token in sent))
+    return sentences
 
-def tokenize(text):
-    """
-        input plain text string,
-        outputs tokenized string with spaces
-    """
-    doc = nlp(text)
-    return " ".join([token.text for token in doc])
-
-def untokenize(tokens):
-    """
-        input tokenized string with spaces
-        outputs plain text string
-    """
-    return TreebankWordDetokenizer().detokenize(tokens.split(" "))
+def untokenize(sentences: list) -> str:
+    return ' '.join(TreebankWordDetokenizer().detokenize(sent) for sent in sentences)
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     if app.config['DEBUG'] == True:
         app.run()
     else:
-        app.run(host="0.0.0.0", port=80)
+        app.run(host="0.0.0.0", port=5000)
