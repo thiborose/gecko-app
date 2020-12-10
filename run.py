dfd46d8b8c780adcbb2a5e3e91eea011a0793dcb
psawa <38402230+psawa@users.noreply.github.com>
2020-12-10 19:49:03 +0100

    fix error during merge

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from application import app
 
 if __name__ == '__main__':
     if app.config['DEBUG'] == True:
+        app.run(debug=True)
     else:
         from waitress import serve
         serve(app, port=80)
