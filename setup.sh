438dcc0612bc9b3cbf344525be2635b0ddb058c3
Eduardo Calò <57707528+Dodo-s95@users.noreply.github.com>
2021-01-02 18:24:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,15 +8,6 @@ else
     (mkdir -p application/models/gector/data/model_files && cd application/models/gector/data/model_files && curl -O https://grammarly-nlp-data-public.s3.amazonaws.com/gector/xlnet_0_gector.th)
 fi
 
-
-FILE=application/models/gector/data/model_files/xlnet_0_gector.th
-if [ -f "$FILE" ]; then
-    echo "$FILE is already downloaded."
-else 
-    (mkdir -p application/models/gector/data/model_files && cd application/models/gector/data/model_files && curl -O https://grammarly-nlp-data-public.s3.amazonaws.com/gector/xlnet_0_gector.th)
-fi
-
-
 # Download sentence reordering models
 FOLDER=application/models/sentence_reorder/model/
 if [ -f "$FOLDER" ]; then
